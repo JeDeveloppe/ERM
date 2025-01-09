d8558251e3a9e79a3b7a6cc5cd2e2bd66dac605e
JeDeveloppe <jedeveloppe.contact@gmail.com>
2025-01-09 21:30:13 +0100

    ok telematic area

--- src/Controller/Admin/TelematicAreaCrudController.php
+++ src/Controller/Admin/TelematicAreaCrudController.php
@@ -3,13 +3,14 @@
 namespace App\Controller\Admin;
 
 use App\Entity\TelematicArea;
+use Doctrine\ORM\QueryBuilder;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
-use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
-use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ColorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 
 class TelematicAreaCrudController extends AbstractCrudController
 {
@@ -24,7 +25,15 @@ class TelematicAreaCrudController extends AbstractCrudController
             AssociationField::new('cgo', 'CGO:'),
             ColorField::new('territoryColor', 'Couleur de la zone:'),
             AssociationField::new('departments', 'Nombre de departements:')->onlyOnIndex(),
-            AssociationField::new('departments', 'Les departements de la zone')->onlyOnForms(),
+            AssociationField::new('departments', 'Les departements de la zone')
+            ->autocomplete()
+            ->onlyOnForms()
+            ->setFormTypeOption('by_reference', false)
+            // ->setQueryBuilder(
+            //     fn(QueryBuilder $queryBuilder) => 
+            //     $queryBuilder
+            //     ->orderBy('entity.name', 'ASC')
+            //     )
         ];
     }
 
